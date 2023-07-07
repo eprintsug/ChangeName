e34ca278ce1c9541bb627a6e7398f1f5a56e0a00
Andrew Mehta <A.J.Mehta@soton.ac.uk>
2023-07-07 10:21:45 +0000

    TVU-196 - Added POD and Version Number. Added boilerplate POD for fleshing out in due course.

--- ScriptOne.pm
+++ ScriptOne.pm
@@ -8,7 +8,58 @@ use     lib dirname(__FILE__).'/../../perl_lib';
 
 use     v5.32;
 
+=pod Name, Version
+
+=encoding utf8
+
+=head1 NAME
+
+Script One.
+
+=head1 VERSION
+
+v1.0.0
+
+=cut
+
+our $VERSION    =   'v1.0.0';
+
+=pod Synopsis, Description
+
+=head1 SYNOPSIS
+
+    # Run at the command line:
+    perl ./ScriptOne.pm
+
+=head1 DESCRIPTION
+
+Put description here.
+
+    # Run from the command line:
+    perl ./ScriptOne.pm
+
+=cut
+
 say hello();
+
+=head1 METHODS
+
+=over
+
+=item hello();
+
+    use ScriptOne qw(hello);
+    my  $greeting = hello();
+    
+    # Alternatively:
+    use ScriptOne;
+    my  $greeting = ScriptOne->hello();
+
+Returns a string containing a greeting.
+
+=back
+
+=cut
 
 sub hello {
     my  $message = "Hello World";
@@ -16,3 +67,41 @@ sub hello {
 }
 
 1;
+
+=head1 AUTHOR
+
+Andrew Mehta
+
+=cut
+
+=head1 COPYRIGHT
+
+=begin COPYRIGHT
+
+Copyright 2023 University of Southampton.
+EPrints 3.4 is supplied by EPrints Services.
+
+http://www.eprints.org/eprints-3.4/
+
+=end COPYRIGHT
+
+=begin LICENSE
+
+This file is part of EPrints 3.4 L<http://www.eprints.org/>.
+
+EPrints 3.4 and this file are released under the terms of the
+GNU Lesser General Public License version 3 as published by
+the Free Software Foundation unless otherwise stated.
+
+EPrints 3.4 is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with EPrints 3.4.
+If not, see L<http://www.gnu.org/licenses/>.
+
+=end LICENSE
+
+__END__
