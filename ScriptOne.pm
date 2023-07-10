0b2f68c887fb0ae4655aa3bd33d92cde86f1bb9a
Andrew Mehta <A.J.Mehta@soton.ac.uk>
2023-07-10 09:54:30 +0000

    TVU-196 - Polished - Ed friendly version for peer review and testing. Removed surplus subroutines, added some POD (my_example still needs more best done once changes to it are finalised), bumped version number.

--- ScriptOne.pm
+++ ScriptOne.pm
@@ -5,8 +5,6 @@ package ScriptOne;
 use     strict;
 use     warnings;
 
-use     File::Basename;
-use     lib dirname(__FILE__).'/../../perl_lib';
 use     Data::Dumper;
 use     EPrints;
 use     EPrints::Repository;
@@ -27,11 +25,11 @@ Script One.
 
 =head1 VERSION
 
-v1.0.0
+v2.0.0
 
 =cut
 
-our $VERSION    =   'v1.0.0';
+our $VERSION    =   'v2.0.0';
 
 =pod Synopsis, Description
 
@@ -77,160 +75,16 @@ sub hello {
 
 =over
 
-=item using_objects_and_methods();
+=item my_example();
 
-Getting familiar with the objects and methods from Ed and Justin's examples.
+    # Run from command line:
+    perl ./ScriptOne.pl
+
+Add description here.
 
 =back
 
 =cut
-
-sub using_objects_and_methods ($self) {
-    say "Do additional says work?";
-    # Find and Change name
-    
-    my  @fields_name_is_found_in = qw(
-        creators
-        contributors
-    );
-    
-    my  @sub_fields = (
-        'family',     # may be part of a name field within creator or contributor
-        'given',    # may be part of a name field within creator or contributor
-        'id',         # id may be a creator or contributor id and not part of a name field
-    );
-    
-    # Currently ignorant of data structure, so let's understand that first.
-
-    # Initial Values:
-    my  $repository_id          =   'initial_archive'; # can later be input
-    my  $dataset_to_use         =   'eprint';
-    my  $compound_name_field    =   'creators_name';
-    my  $search_term            =   'Moghadam';
-    my  $process_results        =   \&process_results;
-
-    #my  @search_values =(
-    #    session =>  $session,
-    #    dataset =>  
-    #);
-
-    my  $session        =   EPrints::Repository->new($repository_id);
-    my  $dataset        =   $session->dataset($dataset_to_use);
-
-    #my  @search_values = (
-    #    ,
-    #);
-
-    #my  %search_options = (
-    #    fields  =>  $compound_name_field,
-    #    value   =>  $search_term,
-    #);
-
-    my  $search         =   EPrints::Search->new(session =>  $session, dataset =>  $dataset);
-
-    $search->add_field($dataset->get_field($compound_name_field), $search_term, 'EQ', 'ANY');
-
-    my  $list_of_results=   $search->perform_search;
-                            
- #   $list_of_results->map(sub {
- #       say "In anon subroutine.";
- #   },
- #   {search_term => $search_term});
-    my  $number_of_search_results = $list_of_results->count;
-    # Let's dump our dataset to understand its data structure.
-    
-#    return "Dataset follows:\n".
-#            Dumper($dataset).
-#            "\nDataset ends.";
-           #'Dataset with-held while debugging signatures.';
-#    return "Dataset fields follow:\n".
-#            Dumper($dataset->fields).
-#            "\nDataset fields end.";
-
-    $session->terminate();
-
-    return "Number of Results follow:\n".
-            $number_of_search_results.
-            "\nResults end.";
-           #"\nEnd.";
-
-}
-
-sub process_results ($session, $dataset, $result, $useful_values) {
-        my $text={
-            output_line => "[%s] %s %s", 
-        };
-
-        say "In Process Results subroutine.";
-
-        foreach my $creators_name ( $result->get_value("creators_name")->@* ) {
-
-            #Definition:
-            #my  $search_term_found = $creators_name->{'family'} eq $useful_values->{'search_term'};
-
-            my  @values = (
-                $result->get_id(),
-                $creators_name->{'given'},
-                $creators_name->{'family'},
-            );
-                
-            #say         sprintf($text->{'output_line'}, @values)
-            #            if $search_term_found;
-            
-            say "Result: ".Dumper(@values);
-        }; 
-        
-};
-
-sub justin_example {
-
-    my  ($term, $repoid) = ('Fam','initial_archive');
-    my $session = EPrints::Session->new( 1 , $repoid , 0 );
-
-    if( !defined $session )
-
-    {
-            say "Failed to load repository: $repoid\n";
-            exit 1;
-    }
-
-    my $ds = $session->dataset( "eprint" );
-
-    die "Non-defined dataset!" if (!defined $ds);
-
-    my  @search_values = (
-        session =>  $session,
-        dataset =>  $ds,
-        satisfy_all => 0,
-    );
-
-    my $searchexp = new EPrints::Search(@search_values);
-
-    my  $field = $ds->get_field( "creators_name" );
-    
-    #say "Field Description: ".Dumper($field->render_description); #can't run method as MetaField::Name not Search::Field
-    
-    my $description = $searchexp->add_field( $field, $term )->render_description;
-
-    my $list = $searchexp->perform_search;
-
-    say "Field Description: ".Dumper($description); # Blessed coderef setting a number??
-
-    say "Number of dataset records found: ".$ds->count($session);
-    say "Number of search results found: ".$list->count;
-
-#    $list->map( sub
-#    {
-#            my( $session, $dataset, $eprint ) = @_;
-#            my @creators_name = @{ $eprint->get_value("creators_name") };
-#            foreach my $cn ( @creators_name )
-#            {
-#                    print "[". $eprint->get_id() . "]\t" . $cn->{given} . " " . $cn->{family} . "\n" if $cn->{family} eq $term;
-#            }
-#    } );
-
-    $session->terminate();
-}
 
 sub my_example {
 
@@ -243,6 +97,7 @@ sub my_example {
         data_count              =>  'Number of dataset records found: ',
         search_count            =>  'Number of search results found: ',
     };
+    my  $line_delimiter         =   "\n";
     my  $search_fields          =   [
                                         {
                                             meta_fields     =>  [
@@ -284,12 +139,43 @@ sub my_example {
 
 
     # Output:
-    say $ARG for $output->{'lines'}->@*;
-    say $text->{'data_count'}.      $counts->{'data'};
-    say $text->{'search_count'}.    $counts->{'search'};
+    push $output->{'lines'}->@*, (
+        $text->{'data_count'}.      $counts->{'data'},
+        $text->{'search_count'}.    $counts->{'search'},
+    );
+    
+    return join $line_delimiter, $output->{'lines'}->@*;
 
-    return "End.";
 }
+
+=over
+
+=item result_processing( $session, $dataset, $result, $output);
+
+Expects an L<EPrints::Repository> object ($session),
+L<EPrints::DataSet> object,
+L<EPrints::List> object (as typically returned from a search),
+and a hash reference of useful information (traditionally $info),
+which in this case will be used 
+to manipulate the value of a lines hash key,
+that stores an array reference to an array of lines,
+to be used for printing output to the terminal,
+and so is described as $output.
+
+Output is expected to be the manipulated $output hash reference,
+so there is no explicit return statement.
+
+Notes:
+Used internally by my_example, 
+so should possibly be made a private method
+(as is possible with lexical methods 
+in L<Object::Pad> or presumably Perl 5.38 too).
+Might go back to making it 
+an anonymous sub / coderef within my_example method.
+
+=back
+
+=cut
 
 sub result_processing ($session, $dataset, $result, $output) {
 
