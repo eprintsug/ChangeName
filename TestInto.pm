91ea25ea267aa92b24a193cbfce7aa33b4ebd811
Andrew <eprints_git@unitedgames.co.uk>
2024-07-19 20:38:51 +0000

    TVU-196 - TestInto - Debugging - Simpler approach works best. Strict, Warnings, Feature Bundle, UTF8 flag all tested. Maybe we should test output encoding too?

--- TestInto.pm
+++ TestInto.pm
@@ -1,67 +1,46 @@
 #!/usr/bin/env perl
 
+# Used throughout:
+use     strict;
 use     warnings;
-use     v5.16; # 
+use     v5.16;
+use     utf8;
 
-package BoilerPlate::PragmasAndEncoding v1.0.0 {
-    use     strict;
-    use     warnings;
-    use     utf8;                                       # This file in utf8.
+# Global Encoding Settings:
+my      $encoding_layer;
 
-    my      $encoding_layer;
-    BEGIN {
-        my  $encoding_to_use    =   'UTF-8';
-            
-        $encoding_layer         =   ":encoding($encoding_to_use)";
-    };
+BEGIN {
+    my  $encoding_to_use    =   'UTF-8';
+        
+    $encoding_layer         =   ":encoding($encoding_to_use)";
+};
 
-    # Global (so not in the import subroutine):
-    use     open ':std'         ,   "$encoding_layer";  # :std affect is global.
-    $ENV{'PERL_UNICODE'}        =   'AS';               # A = Expect @ARGV values to be UTF-8 strings.
-                                                        # S = Shortcut for I+O+E - Standard input, output and error, will be UTF-8.
-                                                        # ENV settings are global for current thread and any forked processes.
-
-    sub import {
-
-        # Processing / Declaring what Pragmas to import:
-        $_->import for qw(
-            strict
-            warnings
-            utf8
-        );
-
-        #Set default on standard input, output, and error:
-        binmode STDIN,  $encoding_layer;
-        binmode STDOUT, $encoding_layer;
-        binmode STDERR, $encoding_layer;
-    
-    }
-    
-    # Protect subclasses using AUTOLOAD
-    sub DESTROY { }
-    
-    1;
-
-} # ChangeNameOperation::BoilerPlate::PragmaAndEncoding Package.
+use     open ':std'         ,   "$encoding_layer";  # :std affect is global.
+$ENV{'PERL_UNICODE'}        =   'AS';               # A = Expect @ARGV values to be UTF-8 strings.
+                                                    # S = Shortcut for I+O+E - Standard input, output and error, will be UTF-8.
+                                                    # ENV settings are global for current thread and any forked processes.
+#Set default on standard input, output, and error:
+binmode STDIN,  $encoding_layer;
+binmode STDOUT, $encoding_layer;
+binmode STDERR, $encoding_layer;
 
 package ChangeNameOperation v1.0.0 {
 
-    BoilerPlate::PragmasAndEncoding->import;
     use     English qw(
                 -no_match_vars
             );                      # Use full english names for special perl variables,
                                     # except the regex match variables
                                     # due to a performance if they are invoked,
                                     # on Perl v5.18 or lower.
+#    use     utf8;
                                     
-    use Data::Dumper;
+    use Data::Dumper;   # Dumper works when local.
     
     print Dumper('what');
     #local $baz = 9;	# Strict works!
-#    use warnings;
-    my $x = "2:" + 3;
-    say "Hello!".$PERL_VERSION;
-    
+    #my $x = "2:" + 3; # Warnings works!
+    say "Hello!".$PERL_VERSION; # English works
+    my $é = 'test'; # utf8 works.
     1;
 
 }; # ChangeNameOperation Package.
