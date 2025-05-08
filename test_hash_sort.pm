908d7e754d6c80f98b211792de3cfbd581645d20
Andrew <eprints_git@unitedgames.co.uk>
2025-05-08 23:12:20 +0100

    German short tokens.

--- test_hash_sort.pm
+++ test_hash_sort.pm
@@ -29,11 +29,48 @@ our $VERSION                            =   'v2.0.6';
 use English;
 
 my %hash                                =   (
-                                                key1    =>  'value',
-                                                key3    =>  'value',
-                                                key2    =>  'value',
-                                                abc     =>  'crikey!',
-                                                xyz     =>  'crikey also!',
+'language.name'                         =>  'Deutsch (Deutschland)',
+'language.error.set_language_handle'    =>  'Probleme beim Finden einer zu verwendenden Sprache.',
+
+'nest.error.language'                   =>  'Ungültiger Sprach-Handle zum Aufrufen der Methode „maketext“.',
+'nest.error.key'                        =>  'Fehler beim Verschachteln eines Lexikonwerts.',
+
+'options.language'                      =>  'sprache spr',
+'options.config'                        =>  'konfig konfiguration',
+'options.live'                          =>  'live',
+'options.verbose'                       =>  'ausführlich ausführl',
+'options.debug'                         =>  'debug',
+'options.trace'                         =>  'stacktrace trace',
+'options.no_dumper'                     =>  'kein_dumper kein_dump keindumper keindump',
+'options.no_trace'                      =>  'kein_stacktrace keinstacktrace kein_trace keintrace',
+'options.exact'                         =>  'exakt genau genaue',
+'options.help'                          =>  'hilfe',
+
+'input.yes_letter'                      =>  'J',
+'input.no_letter'                       =>  'N',
+'input.all'                             =>  'ALLE',
+'input.none'                            =>  'KEINER',
+'input.given'                           =>  'vorname',
+'input.family'                          =>  'familienname',
+'input.1'                               =>  '1',
+'input.2'                               =>  '2',
+
+'name.given'                            =>  'Vorname',
+'name.family'                           =>  'Familienname',
+'name.honourific'                       =>  'Ehrenname',
+'name.lineage'                          =>  'Abstammungsname', # Unsure about this one - it's literally Ancestral Name
+
+'display_line'                          =>  'Datensatz [_1]: [_2].',
+
+'log.type.verbose'                      =>  'ausführlich',
+'log.type.log'                          =>  'protokoll',
+'log.type.debug'                        =>  'debug',
+'log.type.dumper'                       =>  'dumper',
+'log.type.trace'                        =>  'stacktrace',
+
+'finish.change'                         =>  '[quant,_1,Änderung] von [quant,_2,Änderungen] abgeschlossen.',
+'finish.no_change'                      =>  'Keine Änderungen vorgenommen.',
+'finish.thank_you'                      =>  'Vielen Dank, dass Sie dieses Skript verwenden.',
                                             );
 
 my @array                               =   sort {$a cmp $b} keys %hash;
@@ -50,10 +87,56 @@ my  $output                             =   $new_line;
                                             $single_quote.
                                             %hash{$ARG}.
                                             $single_quote.
+                                            ','.
                                             $new_line
     )                                       for @array;
 
 say $output;
 
 __END__
+
+Output edited:
+
+'display_line'                           =>  'Datensatz [_1]: [_2].',
+
+'finish.change'                          =>  '[quant,_1,Änderung] von [quant,_2,Änderungen] abgeschlossen.',
+'finish.no_change'                       =>  'Keine Änderungen vorgenommen.',
+'finish.thank_you'                       =>  'Vielen Dank, dass Sie dieses Skript verwenden.',
+
+'input.1'                                =>  '1',
+'input.2'                                =>  '2',
+'input.all'                              =>  'ALLE',
+'input.family'                           =>  'familienname',
+'input.given'                            =>  'vorname',
+'input.no_letter'                        =>  'N',
+'input.none'                             =>  'KEINER',
+'input.yes_letter'                       =>  'J',
+
+'language.error.set_language_handle'     =>  'Probleme beim Finden einer zu verwendenden Sprache.',
+'language.name'                          =>  'Deutsch (Deutschland)',
+
+'log.type.debug'                         =>  'debug',
+'log.type.dumper'                        =>  'dumper',
+'log.type.log'                           =>  'protokoll',
+'log.type.trace'                         =>  'stacktrace',
+'log.type.verbose'                       =>  'ausführlich',
+
+'name.family'                            =>  'Familienname',
+'name.given'                             =>  'Vorname',
+'name.honourific'                        =>  'Ehrenname',
+'name.lineage'                           =>  'Abstammungsname',  # Unsure about this one - it's literally Ancestral Name
+
+'nest.error.key'                         =>  'Fehler beim Verschachteln eines Lexikonwerts.',
+'nest.error.language'                    =>  'Ungültiger Sprach-Handle zum Aufrufen der Methode „maketext“.',
+
+'options.config'                         =>  'konfig konfiguration',
+'options.debug'                          =>  'debug',
+'options.exact'                          =>  'exakt genau genaue',
+'options.help'                           =>  'hilfe',
+'options.language'                       =>  'sprache spr',
+'options.live'                           =>  'live',
+'options.no_dumper'                      =>  'kein_dumper kein_dump keindumper keindump',
+'options.no_trace'                       =>  'kein_stacktrace keinstacktrace kein_trace keintrace',
+'options.trace'                          =>  'stacktrace trace',
+'options.verbose'                        =>  'ausführlich ausführl',
 
