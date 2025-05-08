32c322c39c81d47a7e482bec27ee6f7d4f1df5b9
Andrew <eprints_git@unitedgames.co.uk>
2025-05-08 22:30:46 +0100

    Add script to test sorting an array

---
+++ test_hash_sort.pm
@@ -0,0 +1,40 @@
+#!/usr/bin/env perl
+
+# Used throughout:
+use     strict;
+use     warnings;
+use     v5.16;
+use     utf8;
+
+# Global Encoding Settings:
+my      $encoding_layer;
+
+SET_ENCODING_LAYER_AT_COMPILE_TIME: BEGIN {
+    my  $encoding_to_use                =   'UTF-8'; # Change this to desired encoding value.
+
+    $encoding_layer                     =   ":encoding($encoding_to_use)";  # This is what actually gets used for the layer.
+};
+
+use     open ':std'                     ,   "$encoding_layer";  # :std affect is global.
+binmode STDIN                           ,   $encoding_layer;
+binmode STDOUT                          ,   $encoding_layer;
+binmode STDERR                          ,   $encoding_layer;
+
+$ENV{'PERL_UNICODE'}                    =   'AS';               # A = Expect @ARGV values to be UTF-8 strings.
+                                                                # S = Shortcut for I+O+E - Standard input, output and error, will be UTF-8.
+                                                                # ENV settings are global for current thread and any forked processes.
+
+our $VERSION                            =   'v2.0.6';
+
+use English;
+
+my @array                               =   (
+                                                key1 =>  'value',
+                                                key3 =>  'value',
+                                                key2 =>  'value',
+                                            );
+                                            
+say $ARG for @array;
+
+__END__
+
